a0c312a87031b104dfb9f00de8ff1bbac1620903
darklord <113107334+Enes-sen@users.noreply.github.com>
2025-09-19 13:22:34 +0300

    Update Of ScrapyExtractor.py

--- ScrapyExtractor.py
+++ ScrapyExtractor.py
@@ -1,17 +1,17 @@
 import requests as Resti
-from ExtractType import ExtractModel
+from ExtractType import AddToTable
 from bs4 import BeautifulSoup as bs
 _url = "https://www.namecheap.com/blog/"
 try:
-    data = Resti.get(_url,timeout=100)
+    data = Resti.get(_url,timeout=250)
     content =  data.content
     soup = bs(content,'html.parser')
     datas = soup.prettify()
-    for ar in soup.find_all("a",class_="sds-blog-card__info-author-link"):
-        for tt in soup.find_all("span",class_="sds-blog-card__title-text"):
-            newdata = ExtractModel()
-            newdata.AddToTable(tt.text,ar.text)
+    
+    for sp in soup.find_all("span",class_="sds-blog-card__title-text"):
+        for atr in  soup.find_all("a",class_="sds-blog-card__info-author-link"):
+            for lnk in soup.find_all("a",class_="sds-blog-card__title"):
+                AddToTable(sp.text,atr.text,lnk["href"])
             
 except BaseException as err:
     print("Something wrong about it:",err.args)
-    
