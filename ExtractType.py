2c9bc1ca4d5e7bcffae2b640b7e45010832f8af9
darklord <113107334+Enes-sen@users.noreply.github.com>
2025-09-19 13:23:25 +0300

    Updated ExtractType.py

--- ExtractType.py
+++ ExtractType.py
@@ -1,15 +1,16 @@
 import sqlite3 as Sq3
 
-class ExtractModel:
-    def __init__(self):
-        Con = Sq3.connect("namecheapBlog.db")
-        cur = Con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS ExtractModel(_id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,author_name TEXT)")
-        Con.commit()
-        Con.close()
-    def AddToTable(self,title,author_name):
-        Con = Sq3.connect("namecheapBlog.db")
-        cur = Con.cursor()
-        cur.execute("INSERT INTO ExtractModel (title, author_name) VALUES (?, ?)", (title, author_name))
-        Con.commit()
-        Con.close()
+def inittilazer():
+    Con = Sq3.connect("namecheapBlog.db")
+    cur = Con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS ExtractModel(_id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,author TEXT,_url TEXT)")
+    Con.commit()
+    Con.close()
+    
+def AddToTable(title,author,_url):
+    inittilazer()
+    Con = Sq3.connect("namecheapBlog.db")
+    cur = Con.cursor()
+    cur.execute("INSERT INTO ExtractModel (title, author,_url) VALUES (?, ?,?)", (title, author,_url))
+    Con.commit()
+    Con.close()
